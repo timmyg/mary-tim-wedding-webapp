1cfa41581e39407d60b2d3d05f63a2e2b378794e
Tim Giblin <timmyg13@gmail.com>
2016-11-22 23:41:20 -0500

    header pix

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -24,7 +24,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
  */
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  title: 'Mary + Tim Giblin Wedding',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
--- src/app/+detail/detail.component.ts
+++ src/app/+detail/detail.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'detail',
-  template: require('./detail.component.pug')
+  template: require('./detail.component.pug'),
+  styleUrls: ['./detail.component.scss']
 })
 export class Detail {
   constructor() {
