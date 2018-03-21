af123bdcf21a92270cc5ca006c988a3699bfeec9
Tim Giblin <timmyg13@gmail.com>
2018-03-20 22:42:59 -0400

    build

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -246,7 +246,7 @@ module.exports = function(options) {
      */
     plugins: [
       new AssetsPlugin({
-        path: helpers.root("dist"),
+        path: helpers.root("docs"),
         filename: "webpack-assets.json",
         prettyPrint: true
       }),
